0760ef6b66daf5a5898c0fa7351762525c4a2a85
Andre <andrejss88@gmail.com>
2025-05-20 12:22:47 +0200

    1812 Determine Color on a Chessboard - Solved

---
+++ src/main/java/com/_2easy/strings/DetermineColorChessboard.java
@@ -0,0 +1,35 @@
+package com._2easy.strings;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 1812
+ * You are given coordinates, a string that represents the coordinates of a square of the chessboard.
+ * Return true if the square is white, and false if the square is black.
+ * <p>
+ * Example 1:
+ * Input: coordinates = "a1"
+ * Output: false
+ */
+public class DetermineColorChessboard {
+
+    static HashSet<Character> whiteOnEven = new HashSet<>(Set.of('a', 'c', 'e', 'g'));
+    static HashSet<Character> whiteOnOdd = new HashSet<>(Set.of('b', 'd', 'f', 'h'));
+
+    public static boolean squareIsWhite(String coordinates) {
+        char letter = coordinates.charAt(0);
+        char number = coordinates.charAt(1);
+
+        // whites on even
+        if (number % 2 == 0 && whiteOnEven.contains(letter)) {
+            return true;
+            // whites on odd
+        } else if (!(number % 2 == 0) && whiteOnOdd.contains(letter)) {
+            return true;
+        }
+
+        // otherwise black
+        return false;
+    }
+}
---
+++ src/test/java/_2easy/strings/DetermineColorChessboardTest.java
@@ -0,0 +1,22 @@
+package _2easy.strings;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static com._2easy.strings.DetermineColorChessboard.squareIsWhite;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DetermineColorChessboardTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "a1, false",
+            "c5, false",
+            "h3, true",
+            "g4, true",
+    })
+    void chessboardTest(String coordinates, boolean expectedResult) {
+        boolean actual = squareIsWhite(coordinates);
+        assertEquals(expectedResult, actual);
+    }
+}
