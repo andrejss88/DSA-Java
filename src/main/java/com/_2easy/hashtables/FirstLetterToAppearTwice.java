c92ffc7cab22bb247f2a140cb9d69ce6ffe54ac7
Andre <andrejss88@gmail.com>
2025-05-20 20:27:43 +0200

    2351 - First Letter to Appear Twice - Solved

---
+++ src/main/java/com/_2easy/hashtables/FirstLetterToAppearTwice.java
@@ -0,0 +1,40 @@
+package com._2easy.hashtables;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 2351
+ * Given a string s consisting of lowercase English letters, return the first letter to appear twice.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: s = "abccbaacz"
+ * Output: "c"
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: s = "abcdd"
+ * Output: "d"
+ */
+public class FirstLetterToAppearTwice {
+
+    public static void main(String[] args) {
+        System.out.println(repeatedCharacter("abccbaacz"));  // c
+        System.out.println(repeatedCharacter("abcdd"));  //d
+    }
+
+    // no tests - easy enough
+    public static char repeatedCharacter(String s) {
+
+        Set<Character> set = new HashSet<>();
+        char letter = ' ';
+        for (int i = 0; i < s.length(); i++) {
+            letter = s.charAt(i);
+            if (!set.add(letter)) {
+                break; // found first duplicate - exit
+            }
+        }
+        return letter;
+    }
+}
