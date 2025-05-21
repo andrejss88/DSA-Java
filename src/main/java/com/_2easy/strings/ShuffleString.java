3ce9a295f12206aab5fe9dc52d50572b83d0e1e2
Andre <andrejss88@gmail.com>
2025-05-21 16:36:31 +0200

    1528 - Shuffle String - Solved

---
+++ src/main/java/com/_2easy/strings/ShuffleString.java
@@ -0,0 +1,30 @@
+package com._2easy.strings;
+
+/**
+ * 1528
+ * Shuffle string letters according to values of int[] indices
+ */
+public class ShuffleString {
+
+    // no tests, easy enough
+    public static void main(String[] args) {
+
+        System.out.println(restoreString("codeleet", new int[]{4, 5, 6, 7, 0, 2, 1, 3}));  // leetcode
+        System.out.println(restoreString("abc", new int[]{0, 1, 2}));                       // abc
+    }
+
+    // 1ms, Beats 69% runtime, 40% memory
+    // Only thin that makes it faster (0ms) in LeetCode is
+    // changing String.valueOf(str) -> new String(str);
+    public static String restoreString(String s, int[] indices) {
+
+        char[] str = new char[indices.length];
+
+        for (int i = 0; i < indices.length; i++) {
+            int newIndex = indices[i];
+            str[newIndex] = s.charAt(i);
+        }
+
+        return String.valueOf(str);
+    }
+}
