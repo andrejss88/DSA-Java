52a56edd47c79d96e8a52295852ae3efcd8b4004
Andre <andrejss88@gmail.com>
2025-06-04 10:30:15 +0200

    Add partial solution to a problem

--- src/main/java/com/_1easy/arrays/matrix/CountNegativeNumbersInSortedMatrix.java
+++ src/main/java/com/_1easy/arrays/matrix/CountNegativeNumbersInSortedMatrix.java
@@ -12,6 +12,27 @@ package com._1easy.arrays.matrix;
  */
 public class CountNegativeNumbersInSortedMatrix {
 
+    // [
+    // [4, 3, 2,-1],
+    // [3, 2, 1,-1],
+    // [1, 1,-1,-2],
+    // [-1,-1,-2,-3]
+    // ]
+
+    public static int countNegativesFaster(int[][] grid) {
+
+        int count = 0;
+
+        // loop over ever row
+
+        // use binary to search for -1
+        // implement logic that is reverse of classic binary search
+        // if -1 is found - add to count from index of -1 to end of arr
+        // if -1 is NOT found - then ?? - loop forward until I find first negative num, and add to count from there to end of arr
+
+
+        return count;
+    }
 
     // 1ms, simple Brute Force, Beats runtime 50%
     public static int countNegatives(int[][] grid) {
